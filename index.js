6670a80909ca3b08273e67b7bbe42c55e4abf430
harun.buljubasic <harun.buljubasic@massiveart.com>
2022-05-20 00:21:40 +0200

    Added basic db connection and created route to post and get messages

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const mongoose = require('mongoose');
+
+
+const port = 3000;
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+var Message = mongoose.model('Message',{
+    name : String,
+    message : String
+});
+
+var dbUrl = 'mongodb://127.0.0.1:27017/chat-app';
 
 app.get('/', (req, res) => {
     res.sendFile('index.html');
 })
 
+app.get('/messages', (req, res) => {
+    Message.find({},(err, messages)=> {
+        res.send(messages);
+    })
+})
+
+app.post('/messages', async (req, res) => {
+    try{
+        const message = new Message(req.body);
+
+        await message.save();
+        console.log('Message saved');
+    }
+    catch (error){
+        res.sendStatus(500);
+        return console.log('error',error);
+    }
+})
+
+mongoose.connect(dbUrl);
+
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 app.listen(port, () => {
-    console.log(`Chat app listening on port ${port}.`)
+    console.log(`Chat app listening on port ${port}.`);
 })
